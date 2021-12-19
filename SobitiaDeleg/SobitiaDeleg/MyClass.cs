f239002cfc9943fadd0c200da9388b93bc2e64b9
Galinka <galinka2002niko@gmail.com>
2021-12-20 00:15:29 +0300

    df

---
+++ SobitiaDeleg/SobitiaDeleg/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SobitiaDeleg
+{
+    class MyClass
+    {
+        //поле  
+        string name;
+
+        //событие  
+        public event MyDel MyEvent;
+
+        //конструктор  
+        public MyClass(string name)
+        {
+            this.name = name;
+        }
+
+        //метод для редактирования поля name  
+        public void ChangeName(string name)
+        {
+            this.name = name;
+            //генерирование события  
+            if (MyEvent != null)
+                MyEvent();
+        }
+    }
+}
+
---
+++ SobitiaDeleg/SobitiaDeleg/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SobitiaDeleg
+{
+    delegate void MyDel();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаем объект класса MyClass  
+            MyClass myClass = new MyClass("Первое имя");
+            //Подписываемся на событие MyEvent  
+            myClass.MyEvent += new MyDel(Handler);
+
+            //Изменяем поле name  
+            myClass.ChangeName("Новое имя");
+        }
+        static void Handler()
+        {
+            Console.WriteLine("Имя изменилось!");
+            Console.ReadKey();
+        }
+    }
+    
+
+}
